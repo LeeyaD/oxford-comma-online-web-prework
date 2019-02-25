46f238c9182fed1ec3dc3246ed8c2d7d4645a7de
Leeya Davis <leeya463@gmail.com>
2019-02-25 22:57:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
+Fruits = ["Apple", "Banana", "Grapefruit", "Watermelon"]
 def oxford_comma(array) #takes an argument array of string elements and converts it into a string using the Oxford comma
 
 end
+# argument 'array' is an array with only string elements
+# call #join on array to convert array to one long string
+# #join needs an argument that separates each work with a comma and a space
