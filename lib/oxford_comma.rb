f0b4b08708a51aaab0898d4a81191dec133b76e4
Leeya Davis <leeya463@gmail.com>
2019-02-25 23:44:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array) #takes an argument array of string elements and converts
     array[0] << " and"
     array.join(" ")
   when array.length >= 3 # 3-element+ array returns returns commas plus final 'and'
-
+    array[array.length - 2] << ", and"
   end
 end
 # call #join on array to convert array to one long string
