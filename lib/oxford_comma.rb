541d05c23e4d7f8ea856deadc6f1b405f1fb00fa
Leeya Davis <leeya463@gmail.com>
2019-02-25 23:21:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ Fruits = ["Apple", "Banana", "Grapefruit", "Watermelon"]
 def oxford_comma(array) #takes an argument array of string elements and converts it into a string using the Oxford comma
   case
   when array.length == 1 # 1-element array returns string w/ no formatting
-    puts array.join
+    array.join
   when array.length == 2 #2-element array returns 'and' between then, single space on either side
   when array.length >= 3 # 3-element+ array returns returns commas plus final 'and'
 
